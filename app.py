eecb17796fbde44c59f8d4338b700a333b1b18d1
udpxcpnevz752-gif <udpxcpnevz752@gmail.com>
2026-05-11 21:39:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
-import threading
-import os
 from dotenv import load_dotenv
+import os
+import admin_bot
 
 load_dotenv()
 
@@ -11,16 +11,6 @@ app = Flask(__name__)
 def home():
     return "Bot is Online!"
 
-def run_telegram_bot():
-    import admin_bot
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-
-    flask_thread = threading.Thread(
-        target=lambda: app.run(host="0.0.0.0", port=port),
-        daemon=True
-    )
-    flask_thread.start()
-
-    run_telegram_bot()
+    app.run(host="0.0.0.0", port=port)
